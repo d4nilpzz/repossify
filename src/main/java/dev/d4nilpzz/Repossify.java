3b3e56e193925efe07eab98effbfa198f66789fa
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2025-12-18 21:32:29 +0100

    chore: Public logger

--- src/main/java/dev/d4nilpzz/Repossify.java
+++ src/main/java/dev/d4nilpzz/Repossify.java
@@ -18,7 +18,7 @@ import java.util.logging.SimpleFormatter;
 public class Repossify {
 
     public static final String VERSION = "1.0.0";
-    private static final Logger logger = Logger.getLogger("Repossify");
+    public static final Logger logger = Logger.getLogger("Repossify");
 
     static {
         ConsoleHandler handler = new ConsoleHandler();

8f8db8bea7cc4228eeed6f4b8306313eec715263
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2025-12-18 22:07:30 +0100

    feat: Use Slf4j with Logback provider (#3)
    * chore: Use Slf4j
    
    * feat: Add logback
    
    * chore: Remove old provider
    
    * feat: Improve logger colors

--- src/main/java/dev/d4nilpzz/RepossifyConfig.java
+++ src/main/java/dev/d4nilpzz/RepossifyConfig.java
@@ -1,5 +1,8 @@
 package dev.d4nilpzz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -7,13 +10,14 @@ import java.nio.file.Path;
 import java.util.Properties;
 
 public class RepossifyConfig {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RepossifyConfig.class);
     private final Properties properties = new Properties();
 
     public RepossifyConfig(Path path) {
         try (FileInputStream fis = new FileInputStream(path.toFile())) {
             properties.load(fis);
         } catch (IOException e) {
-            Repossify.logger.severe("Error loading properties.");
+            LOGGER.error("Error loading properties.");
             throw new RuntimeException(e);
         }
     }
